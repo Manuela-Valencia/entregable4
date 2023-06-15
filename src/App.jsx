57667302d26f6d2dee794220fde173f35f97aae2
Manuela-Valencia <valenciamanuela10@gmail.com>
2023-06-14 20:42:33 -0500

    corrigiendo

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import './App.css'
-import FormUsers from './components/FormUsers.jsx'
-import UsersCard from './components/UserCard.jsx'
+import FormUsers from './components/FormUsers'
+import UsersCard from './components/UserCard'
 import useFetch from './hooks/useFetch'
 
 
